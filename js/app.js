26757a370a9888ec21d99fefdc187666ba25caf3
michaelclement <themichaelclement@gmail.com>
2022-06-15 18:34:23 -0500

    basic interpolation for player movement

--- js/app.js
+++ js/app.js
@@ -12,7 +12,7 @@ class Game {
     main() {
         setInterval(
             this.screen.update,
-            25,
+            20, // updates every 20ms
             this.entities,
             this.screen);
     }
@@ -39,6 +39,7 @@ class Screen {
         self.ctx.clearRect(0, 0, self.canvas.width, self.canvas.height);
 
         entities.forEach(ent => {
+            ent.update();
             self.ctx.drawImage(ent.sprite, ent.x, ent.y);
         })
     }
@@ -46,10 +47,16 @@ class Screen {
 
 class Player {
     ctx;
-    x = 0;
-    y = 0;
     spritePath = "assets/player.png";
     sprite;
+    isAnimating = false;
+    x = 0;
+    y = 0;
+    // move to these coordinates via interpolating
+    dest = {"x": 0, "y": 0};
+    // used to indicate if moving on x or y (for interpolation)
+    axis = null; 
+
     // ctx should be the ctx from our instance of Screen
     constructor(x=0, y=0) {
         this.x = x;
@@ -59,20 +66,43 @@ class Player {
     }
 
     update(dt) {
+        this.interpolateMove();
     }
 
     handleInput(keyCode, sw, sh) {
+        // Ignore input while we're animating
+        if (this.isAnimating) {return;}
+        // If it's an arrow key, we're animating
+        if (keyCode.indexOf("Arrow") > -1) {this.isAnimating = true;}
+
+        // Handle moving in the 4 directions (TODO: less duplication?)
         if (keyCode == 'ArrowLeft' && this.x > 0) {
-            this.x = this.x - 64;
+            this.axis = "x";
+            this.dest["x"] = this.x - 64;
         }
         else if (keyCode == 'ArrowUp' && this.y > 0) {
-            this.y = this.y - 64;
+            this.axis = "y";
+            this.dest["y"] = this.y - 64;
         }
         else if (keyCode == 'ArrowRight' && this.x < (sw - 64)) {
-            this.x = this.x + 64;
+            this.axis = "x";
+            this.dest["x"] = this.x + 64;
         }
         else if (keyCode == 'ArrowDown' && this.y < (sh - 64)) {
-            this.y = this.y + 64;
+            this.axis = "y";
+            this.dest["y"] = this.y + 64;
+        }
+    }
+    interpolateMove() {
+        if (this.isAnimating) {
+            if (this.axis == "x") {
+                this.x = this.x - this.dest["x"] >= 0 ? this.x - 8 : this.x + 8;
+            } else if (this.axis == "y") {
+                this.y = this.y - this.dest["y"] >= 0 ? this.y - 8 : this.y + 8;
+            }
+        }
+        if (this.x == this.dest["x"] && this.y == this.dest["y"])  {
+            this.isAnimating = false;
         }
     }
 }
